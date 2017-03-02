821c9e5bd325edf00703d37c9e65b09f42e8c24b
Tyler Hancock <darklime@live.ca>
2017-03-02 16:59:31 -0700

    Added initial mod code.

---
+++ src/main/java/net/darkhax/sasit/libs/Constants.java
@@ -0,0 +1,15 @@
+package net.darkhax.sasit.libs;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Constants {
+
+    public static final String MODID = "sasit";
+
+    public static final String MOD_NAME = "Shove A Sock In It";
+
+    public static final String VERSION_NUMBER = "1.0.0.0";
+
+    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+}
