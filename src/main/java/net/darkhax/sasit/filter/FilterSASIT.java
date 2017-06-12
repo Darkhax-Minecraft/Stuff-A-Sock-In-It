ffdb0f5ba1ec0a11b5fb46b30a67d938a2137ee2
Tyler Hancock <darklime@live.ca>
2017-06-11 20:31:51 -0600

    Made the Log4J filter use abstract.

--- src/main/java/net/darkhax/sasit/filter/FilterSASIT.java
+++ src/main/java/net/darkhax/sasit/filter/FilterSASIT.java
@@ -2,16 +2,12 @@ package net.darkhax.sasit.filter;
 
 import java.util.logging.LogRecord;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.Logger;
-import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.core.filter.AbstractFilter;
 
 import net.darkhax.sasit.handler.ConfigurationHandler;
 
-public class FilterSASIT implements org.apache.logging.log4j.core.Filter, java.util.logging.Filter {
+public class FilterSASIT extends AbstractFilter implements java.util.logging.Filter {
 
     // Oracle/Java Filter
     @Override
@@ -25,35 +21,5 @@ public class FilterSASIT implements org.apache.logging.log4j.core.Filter, java.u
     public Result filter (LogEvent event) {
 
         return ConfigurationHandler.requiresFiltering("[" + event.getLoggerName() + "]: " + event.getMessage().getFormattedMessage()) ? Result.DENY : Result.NEUTRAL;
-    }
-
-    @Override
-    public Filter.Result filter (Logger logger, Level level, Marker marker, Object object, Throwable t) {
-
-        return Result.NEUTRAL;
-    }
-
-    @Override
-    public Filter.Result filter (Logger logger, Level level, Marker marker, Message message, Throwable t) {
-
-        return Result.NEUTRAL;
-    }
-
-    @Override
-    public Filter.Result getOnMatch () {
-
-        return Result.NEUTRAL;
-    }
-
-    @Override
-    public Filter.Result getOnMismatch () {
-
-        return Result.NEUTRAL;
-    }
-
-    @Override
-    public Result filter (Logger logger, Level level, Marker marker, String msg, Object... params) {
-
-        return Result.NEUTRAL;
     }
 }
